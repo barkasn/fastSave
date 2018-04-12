8d7111e3e72dd461c679542e975717ede51882eb
Nikolas Barkas <nikolas.barkas@outlook.com>
2018-04-12 16:15:35 -0400

    update readme and cleanup

--- README.md
+++ README.md
@@ -13,30 +13,58 @@ Benchmarks were performed on a dual  Intel(R) Xeon(R) E5620  @ 2.40GHz system wi
 The code to run the benchmarks on your system can be found in inst/benchmarks.R
 
 # Installation
-fastSave only works on unix systems and requires the 'pigz' to be installed. On ubuntu you can install 'pigz' with 
-
+fastSave only works on **unix systems** and requires the **pigz** to be installed, to use the lbzip2 functions you will also need **lbzip2** to be installed. On ubuntu you can install the requirements with:
 ```sh
-sudo apt-get install pigz
+sudo apt-get install pigz lbzip2
 ```
 
-To install fastSave, open R and run:
-
+To install the fastSave R packages type the following on the R command prompt:
 ```R
 library(devtools)
 install_github('barkasn/fastSave')
 ```
 ## Use
-To use fast save simply load the package and replace your calls to save() and save.image() with save.fast() and save.image.fast().
+fastSave provides the following  functions:
+
+* save.fast()
+* save.image.fast()
+* save.lbzip2()
+* save.image.lbzip2()
+* load.lbzip2()
+
+save.fast() and save.image.fast() produce files compatible with the standard load() function.  The save.lbzip2() and save.image.lbzip2() functions produce files that can be loaded with load.lbzip2(). Their advantage is that they allow the load operation to be parallelized as well.
+
+To use fastSave simply load the package and replace your calls to save() and save.image() with save.fast() and save.image.fast(). You can use the n.cores argument to provide the number of cores to use. The default is 4 as most modern systems have at least 4 cores and saturation occurs beyond 8 cores anyway.
 ```R
+## load the library
 library(fastSave)
-x <- 5
-save.image.fast()
-save.fast(x)
+
+## Make an object
+x <- rep(0,100)
+
+## Save current enviromnment with function compatible with built-in save() and load()
+## by default it will use ".RData" as a file
+save.image.fast(file="mydata.RData", n.cores = 8)
+
+## Save selected object only in "mydata.RData"
+save.fast(x, file="x.RData",n.cores = 8)
+
+## These files can be loaded with the normal load() function
+load("x.RData")
 ```
 
-You can optionally specify the number of cores to use with the n.cores argument:
 ```R
-library(fastSave)
-x <- 5
-save.image.fast(n.cores = 8)
+## Alternatively you can use the .lbzip2 functions. These produce files that are
+## not compatible with the built in R functions but allow parallel loading.
+## Note that saving with the lbzip2 functions takes slightly longer that
+## with the functions above, but loading time is dramatically shorter.
+
+## The use of the suffix .RDataFS is recommened for these files
+## By default saves into files ".RDataFS"
+save.image.lbzip2("mydata.RDataFS")
+save.lbzip2(x, file="mydata.RDataFS")
+
+## Load with selected function
+load.lbzip2("mydata.RDataFS")
 ```
+
--- inst/benchmarks/benchmarks.R
+++ inst/benchmarks/benchmarks.R
@@ -1,4 +1,4 @@
-## A script to produce benchmark plots
+## Original benchmarks
 
 library(tibble)
 library(tidyverse)
@@ -115,4 +115,139 @@ ggsave('vignettes/figures/ncores.vs.time.png',width=10,height=8)
 ## calculate time reduction
 as.tibble(allres) %>% filter(data.size==4, n.cores %in% c('internal','6')) %>%
   group_by(n.cores) %>% summarise(mean(time))
+
+
+###################################################
+## lbzip2 benchmarks
+
+## A script to produce benchmark plots
+
+library(tibble)
+library(tidyverse)
+
+
+getSavingTime <- function(data.size,n.cores) {
+  #a <- runif(data.size)
+  a <- rep(1.0, data.size)
+  system.time(save.lbzip2(a,file='dummy.RData',n.cores=n.cores))[3]
+}
+
+testSavingTime <- function(settle.time = 0.2, nrep=2, data.start = 0.1, data.end = 0.4, data.step = 0.1,
+                           n.core.start=1,n.core.end=8,n.core.step=1) {
+  gb.size <- 1.34e8
+  data.size.seq <- seq(data.start,data.end,data.step)
+  n.core.seq <- seq(n.core.start,n.core.end,n.core.step)
+  nrep.seq <- seq(1,nrep)
+  params <- expand.grid(data.size.seq, n.core.seq, nrep.seq)
+  results <- rep(0, nrow(params))
+  colnames(params) <- c('data.size','n.cores')
+  for (i in 1:nrow(params)) {
+    cat(paste0('(',i,'/',nrow(params),'): '))
+    cat(paste0('Running with ',params[i,1], ' GB and ',params[i,2],' core(s)... '))
+    results[i] <- getSavingTime(params[i,1] * gb.size, params[i,2])
+    cat(paste0(results[i],'\n'))
+    Sys.sleep(settle.time)
+  }
+  cbind(params, time=results)
+}
+
+## fast test
+fast.test.results <- testSavingTime(settle.time = 0.1, nrep = 1, data.start = 0.1, data.end = 0.6, data.step = 0.2,
+                                    n.core.start = 2, n.core.end = 6, n.core.step = 1)
+saveRDS(fast.test.results, 'inst/benchmarks/lbzip2.save.fast.test.results.RData')
+
+
+### Plotting Fast test
+library(ggplot2)
+ggplot(fast.test.results, aes(x=data.size,y=time,color=factor(n.cores))) +  geom_point()
+ggplot(fast.test.results, aes(color=factor(data.size),y=time,x=n.cores)) +  geom_point()
+
+## slow test
+slow.test.results <- testSavingTime(settle.time = 0.1, nrep = 1, data.start = 0.5, data.end = 5.0, data.step = 0.5,
+                                    n.core.start = 1, n.core.end = 8, n.core.step = 1)
+
+saveRDS(slow.test.results, 'inst/benchmarks/lbzip2.save.slow.test.results.RData')
+
+## Plotting slow test
+ggplot(slow.test.results, aes(x=data.size,y=time,color=factor(n.cores))) +  geom_point()
+ggplot(slow.test.results, aes(color=factor(data.size),y=time,x=n.cores)) +  geom_point()
+
+
+
+#########################################################################
+## compare original and lbzip2
+
+lbzip2.bench <- readRDS('inst/benchmarks/lbzip2.save.slow.test.results.RData')
+internal.bench <- readRDS('inst/benchmarks/internal.test.results.RData')
+original.bench <- readRDS('inst/benchmarks/slow.test.results.RData')
+
+head(lbzip2.bench)
+colnames(lbzip2.bench) <- c('data.size','n.cores','rep','time')
+lbzip2.bench$type <- c('lbzip2')
+
+head(internal.bench)
+colnames(internal.bench) <- c('data.size','rep','time')
+internal.bench$n.cores <- c(1)
+internal.bench <- internal.bench[,c('data.size','n.cores','rep','time')]
+internal.bench$type <- c('internal')
+
+head(original.bench)
+colnames(original.bench) <- c('data.size','n.cores','rep','time')
+original.bench$type <- c('pgiz')
+
+head(lbzip2.bench)
+head(internal.bench)
+head(original.bench)
+
+all.bench <- rbind(lbzip2.bench,internal.bench,original.bench)
+
+head(all.bench)
+library(tidyverse)
+all.bench %>% ggplot(aes(x=data.size,y=time,color=factor(type))) + geom_point() + facet_grid(~n.cores)
+
+all.bench %>%
+  filter(type == 'internal' | (type=='pgiz' & n.cores==8) | (type=='lbzip2' & n.cores==8)) %>%
+  ggplot(aes(x=data.size,y=time,color=factor(type))) + geom_point()
+
+
+
+###########################################################################
+## Loading benchmarks
+## A script to produce benchmark plots
+
+library(tibble)
+library(tidyverse)
+
+
+getLoadingTime <- function(data.size,n.cores) {
+  #a <- runif(data.size)
+  a <- rep(1.0, data.size)
+  save.lbzip2(a,file='dummy.RData',n.cores=8)
+  system.time(load.lbzip2(file='dummy.RData',n.cores=n.cores))[3]
+}
+
+testLoadingTime <- function(settle.time = 0.2, nrep=2, data.start = 0.1, data.end = 0.4, data.step = 0.1,
+                            n.core.start=1,n.core.end=8,n.core.step=1) {
+  gb.size <- 1.34e8
+  data.size.seq <- seq(data.start,data.end,data.step)
+  n.core.seq <- seq(n.core.start,n.core.end,n.core.step)
+  nrep.seq <- seq(1,nrep)
+  params <- expand.grid(data.size.seq, n.core.seq, nrep.seq)
+  results <- rep(0, nrow(params))
+  colnames(params) <- c('data.size','n.cores')
+  for (i in 1:nrow(params)) {
+    cat(paste0('(',i,'/',nrow(params),'): '))
+    cat(paste0('Running with ',params[i,1], ' GB and ',params[i,2],' core(s)... '))
+    results[i] <- getLoadingTime(params[i,1] * gb.size, params[i,2])
+    cat(paste0(results[i],'\n'))
+    Sys.sleep(settle.time)
+  }
+  cbind(params, time=results)
+}
+
+## fast test
+fast.test.results <- testLoadingTime(settle.time = 1, nrep = 2, data.start = 0.5, data.end = 4, data.step = 1,
+                                     n.core.start = 1, n.core.end = 4, n.core.step = 1)
+
+
 
